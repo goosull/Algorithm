a41f2b7940f80e7e2eddcc653ca57d1336630936
김승원 / Seungwon Kim <2060won@naver.com>
2024-09-18 19:57:21 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long N, K;
+
+int main() {
+    cin >> N >> K;
+
+    int temp;
+
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        cin >> temp;
+    }
+
+    cout << "비와이";
+
+    return 0;
+}
