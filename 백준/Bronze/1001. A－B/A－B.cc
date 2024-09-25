98efa5ea01504e6c98756b59c05b04601cd79951
김승원 / Seungwon Kim <2060won@naver.com>
2024-09-25 09:31:47 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  int a, b;
+  cin >> a >> b;
+
+  cout << a - b;
+  return 0;
+}
