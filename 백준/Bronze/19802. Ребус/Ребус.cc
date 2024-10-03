e786266949a0fce29044901f3f11665516045a0b
김승원 / Seungwon Kim <2060won@naver.com>
2024-10-03 22:04:52 +0900

    [Bronze I] Title: Ребус, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/19802. Ребус/Ребус.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    string s, ans;
+
+    while (1) {
+        cin >> s;
+        if (cin.eof()) break;
+
+        int front = 0, end = 0, mid = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '\'' && mid == 0) {
+                front++;
+            } else if (s[i] == '\'' && mid != 0) {
+                end++;
+            } else {
+                mid++;
+            }
+        }
+
+        ans += s.substr(front * 2, s.length() - 2 * front - 2 * end);
+    }
+    cout << ans;
+}
