e851d1ea16d6bd3266904005d8116fdf1ce2d20a
김승원 / Seungwon Kim <2060won@naver.com>
2024-10-03 20:59:44 +0900

    [Bronze I] Title: 애너그램, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/6996. 애너그램/애너그램.cc
+++ 백준/Bronze/6996. 애너그램/애너그램.cc
@@ -1,6 +1,7 @@
+
 #include <algorithm>
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
 int main() {
@@ -9,33 +10,29 @@ int main() {
     std::cout.tie(NULL);
 
     int n;
-
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         string a, b;
         cin >> a >> b;
-        vector<char> aa, bb;
 
-        for (int j = 0; j < a.length(); j++) {
-            aa.push_back(a[j]);
-        }
-        for (int j = 0; j < b.length(); j++) {
-            bb.push_back(b[j]);
-        }
-
-        for (int j = 0; j < aa.size(); j++) {
-            if (find(bb.begin(), bb.end(), aa[j]) != bb.end()) {
-                bb.erase(find(bb.begin(), bb.end(), aa[j]));
-                aa.erase(aa.begin() + j);
-                j--;
-            }
-        }
-
-        if (aa.empty() && bb.empty()) {
-            cout << a << " & " << b << " are anagrams." << endl;
-        } else {
+        // 문자열의 길이가 다르면 애너그램이 될 수 없음
+        if (a.length() != b.length()) {
             cout << a << " & " << b << " are NOT anagrams." << endl;
+            continue;
+        }
+
+        // 두 문자열을 정렬
+        string tempa = a, tempb = b;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        if (a == b) {
+            cout << tempa << " & " << tempb << " are anagrams." << endl;
+        } else {
+            cout << tempa << " & " << tempb << " are NOT anagrams." << endl;
         }
     }
+
+    return 0;
 }
