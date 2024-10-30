0acb334597fbf0c0084829d67acc359cc0e1e5e3
김승원 / Seungwon Kim <2060won@naver.com>
2024-10-30 09:24:47 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.py
@@ -0,0 +1,9 @@
+import sys
+input = lambda: sys.stdin.readline().strip("\n")
+ini = lambda: int(input())
+ins = lambda: [*map(int, input().split())]
+inf = float('inf')
+
+n,m = ins()
+
+print(n + m)
