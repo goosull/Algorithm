43be1b67ad806adebdc87d8a0133d70fe7d5b328
김승원 / Seungwon Kim <2060won@naver.com>
2024-11-18 13:24:52 +0900

    [Silver II] Title: Payback, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/6106. Payback/Payback.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, pos, cur, cnt;
+vector<int> v;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+
+  v.resize(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> v[i];
+    cur += v[i];
+    if (cur < 0) {
+      if (pos == -1) pos = i;
+    } else if (pos != -1) {
+      // cout << "pos: " << pos << " i: " << i << '\n';
+      cnt += 2 * (i - pos);
+      pos = -1;
+    }
+    cnt++;
+  }
+
+  cout << cnt << '\n';
+}
