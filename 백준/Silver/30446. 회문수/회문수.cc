6eabbcfd72749d80f7f19a7c6967c0ccfb422203
김승원 / Seungwon Kim <2060won@naver.com>
2024-11-22 14:31:04 +0900

    [Silver II] Title: 회문수, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/30446. 회문수/회문수.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, ans;
+
+void dfs(string s, int l, bool odd) {
+  if (s.length() > (l + 1) / 2) return;
+
+  string revs = s;
+  if (odd) revs.pop_back();
+  reverse(revs.begin(), revs.end());
+
+  string palindrome = s + revs;
+
+  try {
+    long long num = stoll(palindrome);
+    if (num > n) return;
+    ans++;
+  } catch (const std::exception& e) {
+    return;
+  }
+
+  for (char c = '0'; c <= '9'; c++) {
+    dfs(s + c, l, odd);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+  string strn = to_string(n);
+
+  for (bool odd : {true, false}) {
+    for (char c = '1'; c <= '9'; c++) {
+      dfs(string(1, c), strn.size(), odd);
+    }
+  }
+
+  cout << ans << '\n';
+  return 0;
+}
