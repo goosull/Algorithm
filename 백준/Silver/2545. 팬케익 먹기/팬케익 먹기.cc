f22d00b8a48b57326a055924e85ab04ca0e808e3
김승원 / Seungwon Kim <2060won@naver.com>
2024-12-05 14:08:09 +0900

    [Silver IV] Title: 팬케익 먹기, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cc
+++ 백준/Silver/2545. 팬케익 먹기/팬케익 먹기.cc
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long n, a, b, c, d;
+long long n, d;
+vector<long long> v(3);
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -11,20 +12,36 @@ int main() {
   cin >> n;
 
   for (long long j = 0; j < n; j++) {
-    cin >> a >> b >> c >> d;
+    cin >> v[0] >> v[1] >> v[2] >> d;
 
-    for (long long i = 0; i < d; i++) {
-      long long m = max(a, max(b, c));
+    sort(v.begin(), v.end());
 
-      if (m == a) {
-        a--;
-      } else if (m == b) {
-        b--;
+    long long firstminussecond = v[2] - v[1];
+    long long secondminusthird = v[1] - v[0];
+    long long firstminusthird = v[2] - v[0];
+
+    if (firstminussecond >= d) {
+      cout << (v[2] - d) * v[1] * v[0] << '\n';
+    } else if (firstminusthird + secondminusthird >= d) {
+      v[2] -= firstminussecond;
+      d -= firstminussecond;
+      if (d % 2 == 0) {
+        cout << (v[2] - d / 2) * (v[1] - d / 2) * v[0] << '\n';
       } else {
-        c--;
+        cout << (v[2] - d / 2) * (v[1] - d / 2 - 1) * v[0] << '\n';
+      }
+    } else {
+      v[2] -= firstminusthird;
+      v[1] -= secondminusthird;
+      d -= (firstminusthird + secondminusthird);
+      if (d % 3 == 0) {
+        cout << (v[2] - d / 3) * (v[1] - d / 3) * (v[0] - d / 3) << '\n';
+      } else if (d % 3 == 1) {
+        cout << (v[2] - d / 3) * (v[1] - d / 3) * (v[0] - d / 3 - 1) << '\n';
+      } else {
+        cout << (v[2] - d / 3) * (v[1] - d / 3 - 1) * (v[0] - d / 3 - 1)
+             << '\n';
       }
     }
-
-    cout << a * b * c << '\n';
   }
 }
