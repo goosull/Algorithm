b6f674eee8413795230a75e2c7a0adf73b37957b
김승원 / Seungwon Kim <2060won@naver.com>
2024-12-06 14:42:54 +0900

    [Silver I] Title: 碁石ならべ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/5581. 碁石ならべ/碁石ならべ.cc
+++ 백준/Silver/5581. 碁石ならべ/碁石ならべ.cc
@@ -1,61 +1,48 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  int n;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n, ans = 0;
+  vector<pair<int, int>> board;
+
   cin >> n;
 
-  stack<pair<int, int>>
-      table;  // 스택으로 연속된 색상과 개수를 저장 (색상, 개수)
+  for (int i = 1; i <= n; i++) {
+    int dol;
+    cin >> dol;
 
-  for (int i = 0; i < n; ++i) {
-    int c;
-    cin >> c;
-
-    if (i % 2 == 0) {
-      // 홀수 번째 돌은 그냥 추가
-      if (!table.empty() && table.top().first == c) {
-        // 같은 색상이면 개수 증가
-        table.top().second++;
-      } else {
-        // 새로운 색상을 추가
-        table.push({c, 1});
-      }
+    if (i % 2 == 1) {
+      if (!board.empty() && board.back().first == dol)
+        board.back().second++;
+      else
+        board.push_back({dol, 1});
+    } else if (board.back().first == dol) {
+      board.back().second++;
     } else {
-      // 짝수 번째 돌
-      if (!table.empty() && table.top().first == c) {
-        // 같은 색상이면 개수 증가
-        table.top().second++;
+      int cnt = 1;
+
+      while (!board.empty() && board.back().first != dol) {
+        cnt += board.back().second;
+        board.pop_back();
+      }
+
+      if (!board.empty() && board.back().first == dol) {
+        board.back().second += cnt;
       } else {
-        // 색상이 다를 경우
-        int old_count = 0;
-
-        // 연속된 같은 색 돌 제거
-        while (!table.empty() && table.top().first != c) {
-          old_count += table.top().second;
-          table.pop();
-        }
-
-        // 새로운 색상 돌 추가 (old_count + 1)
-        if (!table.empty() && table.top().first == c) {
-          table.top().second += old_count + 1;
-        } else {
-          table.push({c, old_count + 1});
-        }
+        board.push_back({dol, cnt});
       }
     }
   }
 
-  // 테이블에 남아있는 흰 돌(0)의 개수 계산
-  int whiteCount = 0;
-  while (!table.empty()) {
-    if (table.top().first == 0) {
-      whiteCount += table.top().second;
+  for (int i = 0; i < board.size(); i++) {
+    if (board[i].first == 0) {
+      ans += board[i].second;
     }
-    table.pop();
   }
 
-  cout << whiteCount << endl;
-  return 0;
+  cout << ans << '\n';
 }
